788cb357a26dea8057079feac9e314c3e7a936b5
Abhav <thakur.abhav25@gmail.com>
2021-05-21 13:10:39 +0530

    details

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const express = require("express");
 
 const app = express();
+
 const importData = require("./data.json");
+
+const  scope = require("./scope.json");
+const sense = require("./sense.json");
+const  select = require("./select.json");
+const sce = require("./sce.json");
+const  smec = require("./smec.json");
 
 let port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("hello world!");
+  res.send("hello welcome to abhav api!");
 });
 
 app.get("/players", (req, res) => {
   res.send(importData);
+});
+
+app.get("/sense", (req, res) => {
+  res.send(sense);
+});
+app.get("/scope", (req, res) => {
+  res.send(scope);
+});
+app.get("/select", (req, res) => {
+  res.send(select);
+});
+app.get("/sce", (req, res) => {
+  res.send(sce);
+});
+app.get("/smec", (req, res) => {
+  res.send(smec);
 });
 
 app.listen(port, () => {
